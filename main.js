f466ae8151f1c1c2c4df49ea34cbc355bb1f8faa
rayanical <rayanc2005@gmail.com>
2022-02-24 18:33:52 -0500

    age fix

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ setInterval(add, 1000);
 
 function add() {
     let now = Date.now() - dob.getTime();
-    let smoke_length = now / 1000 - 756864000;
+    let smoke_length = now / 1000 - 819936000;
     count = smoke_length * 0.00015578703;
     cost.innerHTML = count.toLocaleString('en-US', { maximumFractionDigits: 2 });
     title.innerHTML = '$' + count.toLocaleString('en-US', { maximumFractionDigits: 2 });
@@ -18,7 +18,7 @@ setInterval(invest, 1000);
 
 function invest() {
     let now1 = Date.now() - dob.getTime();
-    let smoke_length1 = now1 / 1000 - 756864000;
+    let smoke_length1 = now1 / 1000 - 819936000;
     let years = smoke_length1 / 31536000;
     let final = 13.46 * 1.105 ** years + (4916.16 * (1.105 ** years - 1)) / 0.105;
     investment.innerHTML = final.toLocaleString('en-US', { maximumFractionDigits: 2 });
